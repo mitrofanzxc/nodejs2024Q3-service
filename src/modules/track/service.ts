97911d2111a0225509b2d855c086bac9351a0ead
mitrofanzxc <mitrofanzxc@gmail.com>
2024-11-18 21:03:03 +0300

    feat: prisma to track service

--- src/modules/track/service.ts
+++ src/modules/track/service.ts
@@ -1,65 +1,80 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
-import { DB_TOKEN } from 'src/database/database-token';
+import { PrismaService } from 'src/modules/prisma/service';
 
-import { addEntityToCollection } from 'src/utils/add-entity-to-collection';
-import { deleteEntityFromCollection } from 'src/utils/delete-entity-from-collection';
-import { deleteIDFromFavsCollection } from 'src/utils/delete-id-from-favs-collection';
-import { getEntityByID } from 'src/utils/get-entity-by-id';
-import { isIDValid } from 'src/utils/is-id-valid';
-import { updateEntityInCollection } from 'src/utils/update-entity-in-collection';
-import { validateIDFormat } from 'src/utils/validate-id-format';
-
-import { Track } from 'src/types/interfaces';
-import { DatabaseInterface } from 'src/database/database';
 import { CreateTrackDTO, UpdateTrackDTO } from './types';
 
 @Injectable()
 export class TrackService {
-    constructor(@Inject(DB_TOKEN) private readonly database: DatabaseInterface) {}
+    constructor(private prisma: PrismaService) {}
 
-    private isInvalidDto(dto: CreateTrackDTO | UpdateTrackDTO) {
-        return !dto.name || !isIDValid(dto.albumId) || !isIDValid(dto.artistId);
+    private async getExistedTrack(id: string) {
+        const track = await this.prisma.track.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!track) {
+            throw new NotFoundException('Track with id ${id} not found');
+        }
+
+        return track;
     }
 
     async getTracks() {
-        return this.database.tracks;
+        const tracks = await this.prisma.track.findMany();
+
+        return tracks;
     }
 
     async getTrackById(id: string) {
-        return getEntityByID<Track>(id, this.database.tracks);
+        const track = await this.getExistedTrack(id);
+
+        if (track) {
+            return track;
+        }
     }
 
     async createTrack(createTrackDto: CreateTrackDTO) {
-        if (this.isInvalidDto(createTrackDto)) {
-            throw new BadRequestException(
-                'Request body does not contain required fields or their format is not correct',
-            );
-        } else {
-            return addEntityToCollection(createTrackDto, this.database.tracks);
-        }
+        const newTrack = await this.prisma.track.create({
+            data: {
+                id: uuidv4(),
+                ...createTrackDto,
+            },
+        });
+
+        return newTrack;
     }
 
     async deleteTrack(id: string) {
-        deleteEntityFromCollection(id, this.database.tracks);
-        deleteIDFromFavsCollection(id, this.database.favs.tracks);
+        const track = await this.getExistedTrack(id);
+
+        if (track) {
+            await this.prisma.track.delete({
+                where: {
+                    id,
+                },
+            });
+        }
     }
 
     async updateTrack(updateTrackDto: UpdateTrackDTO, id: string) {
-        if (this.isInvalidDto(updateTrackDto)) {
-            throw new BadRequestException(
-                'Request body does not contain required fields or their format is not correct',
-            );
+        const track = await this.getExistedTrack(id);
+
+        if (track) {
+            const updatedTrack = await this.prisma.track.update({
+                where: {
+                    id,
+                },
+                data: {
+                    ...track,
+                    ...updateTrackDto,
+                },
+            });
+
+            return updatedTrack;
         }
-
-        validateIDFormat(id);
-
-        const updatedTrack = updateEntityInCollection<Track>(
-            id,
-            updateTrackDto,
-            this.database.tracks,
-        );
-
-        return updatedTrack;
     }
 }
